d1cfa832006b380ce87cb0757e41f973362f7590
irokonlywork <irokonlywork@users.noreply.github.com>
2020-05-22 22:13:51 +0300

    Фикстуры: инициализация и зачистка

--- addressbook-web-tests/src/test/java/ru/stqa/course/addressbook/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/course/addressbook/GroupCreationTests.java
@@ -18,10 +18,6 @@ public class GroupCreationTests {
   public void setUp() throws Exception {
     wd = new ChromeDriver();
     wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-  }
-
-  @Test
-  public void testGroupCreation() throws Exception {
     wd.get("https://localhost/addressbook/index.php");
     wd.findElement(By.name("user")).clear();
     wd.findElement(By.name("user")).sendKeys("admin");
@@ -29,6 +25,10 @@ public class GroupCreationTests {
     wd.findElement(By.name("pass")).clear();
     wd.findElement(By.name("pass")).sendKeys("secret");
     wd.findElement(By.xpath("//input[@value='Login']")).click();
+  }
+
+  @Test
+  public void testGroupCreation() throws Exception {
     wd.findElement(By.linkText("groups")).click();
     wd.findElement(By.name("new")).click();
     wd.findElement(By.name("group_name")).click();
