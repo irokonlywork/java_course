5903a0aac926507710a14b35857945cd7ec7a3de
irokonlywork <irokonlywork@users.noreply.github.com>
2020-06-06 14:52:15 +0300

    Количество элементов в коллекции

--- addressbook-web-tests/src/test/java/ru/stqa/course/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/course/addressbook/tests/GroupCreationTests.java
@@ -1,13 +1,17 @@
 package ru.stqa.course.addressbook.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.course.addressbook.model.GroupData;
 
 public class GroupCreationTests extends TestBase {
 
   @Test
-  public void testGroupCreation() throws Exception {
+  public void testGroupCreation() {
     app.getNavigationHelper().gotoGroupPage();
+    int before = app.getGroupHelper().getGroupCount();
     app.getGroupHelper().createGroup(new GroupData("test1", null, null));
+    int after = app.getGroupHelper().getGroupCount();
+    Assert.assertEquals(after, before + 1);
   }
 }
