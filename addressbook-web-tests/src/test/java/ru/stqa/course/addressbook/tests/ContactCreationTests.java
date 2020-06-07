84f9d663ff226c49e022d8b879d3aec32299c391
irokonlywork <irokonlywork@users.noreply.github.com>
2020-06-07 19:50:03 +0300

    Сортировка списков

--- addressbook-web-tests/src/test/java/ru/stqa/course/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/course/addressbook/tests/ContactCreationTests.java
@@ -1,5 +1,6 @@
 package ru.stqa.course.addressbook.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.course.addressbook.model.ContactData;
 
@@ -7,7 +8,10 @@ public class ContactCreationTests extends TestBase {
 
   @Test
   public void testContactCreation() throws Exception {
+    int before = app.getContactHelper().getContactCount();
     app.getContactHelper().createContact(new ContactData("Irok", "Test", "test@gmail.com", "test1"),true);
+    int after = app.getContactHelper().getContactCount();
+    Assert.assertEquals(after, before + 1);
   }
 
 }
