bd9ec4699fd23f8b206095cff398462f00e6ff4b
irokonlywork <irokonlywork@users.noreply.github.com>
2020-06-21 00:34:04 +0300

    Параметризован тест для создания контактов

--- addressbook-web-tests/src/test/java/ru/stqa/course/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/course/addressbook/tests/ContactCreationTests.java
@@ -1,26 +1,47 @@
 package ru.stqa.course.addressbook.tests;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.course.addressbook.model.ContactData;
 import ru.stqa.course.addressbook.model.Contacts;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class ContactCreationTests extends TestBase {
 
-  @Test
-  public void testContactCreation() throws Exception {
-    Contacts before = app.contact().all();
-    File photo = new File("src/test/resources/qaz.jpg");
-    ContactData contact = new ContactData()
-            .withFirstname("Irok").withLastname("Test").withEmail("new@gmail.com").withPhoto(photo);
-    app.contact().create(contact,true);
-    Contacts after = app.contact().all();
-    assertThat(after.size(), equalTo(before.size() + 1));
-    assertThat(after, equalTo(
-            before.withAdded(contact.withId(after.stream().mapToInt((c) -> c.getId()).max().getAsInt()))));
-  }
+    @DataProvider
+    public Iterator<Object[]> validContactsFromJson() throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.json")));
+        String json = "";
+        String line = reader.readLine();
+        while (line != null) {
+            json += line;
+            line = reader.readLine();
+        }
+        Gson gson = new Gson();
+        List<ContactData> contacts = gson.fromJson(json, new TypeToken<List<ContactData>>() {}.getType()); // List<ContactData>.class
+        return contacts.stream().map((contactData -> new Object[]{contactData})).collect(Collectors.toList()).iterator();
+    }
+
+    @Test(dataProvider = "validContactsFromJson")
+    public void testContactCreation(ContactData contact) throws Exception {
+        Contacts before = app.contact().all();
+        File photo = new File("src/test/resources/qaz.jpg");
+        app.contact().create(contact, true);
+        Contacts after = app.contact().all();
+        assertThat(after.size(), equalTo(before.size() + 1));
+        assertThat(after, equalTo(
+                before.withAdded(contact.withId(after.stream().mapToInt((c) -> c.getId()).max().getAsInt()))));
+    }
 }
