061efebc8b6ffeeaef3859b3e977229ac7928613
irokonlywork <irokonlywork@users.noreply.github.com>
2020-07-05 16:03:40 +0300

    Подготовка проекта для тестирования MantisBT

--- mantis-tests/src/test/java/ru/stqa/course/mantis/appmanager/ApplicationManager.java
+++ mantis-tests/src/test/java/ru/stqa/course/mantis/appmanager/ApplicationManager.java
@@ -1,6 +1,5 @@
-package ru.stqa.course.addressbook.appmanager;
+package ru.stqa.course.mantis.appmanager;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -18,12 +17,7 @@ public class ApplicationManager {
     private final Properties properties;
     WebDriver wd;
 
-    private ContactHelper contactHelper;
-    private SessionHelper sessionHelper;
-    private NavigationHelper navigationHelper;
-    private GroupHelper groupHelper;
     private String browser;
-    private DbHelper dbHelper;
 
     public ApplicationManager(String browser) {
         this.browser = browser;
@@ -34,8 +28,6 @@ public class ApplicationManager {
         String target = System.getProperty("target", "local");
         properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
 
-        dbHelper = new DbHelper();
-
         if ( browser.equals(BrowserType.CHROME) ) {
             wd = new ChromeDriver();
         } else if ( browser.equals(BrowserType.FIREFOX) ) {
@@ -45,38 +37,9 @@ public class ApplicationManager {
         }
         wd.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
         wd.get(properties.getProperty("web.baseUrl"));
-        groupHelper = new GroupHelper(wd);
-        navigationHelper = new NavigationHelper(wd);
-        sessionHelper = new SessionHelper(wd);
-        contactHelper = new ContactHelper(wd);
-        sessionHelper.login(properties.getProperty("web.adminLogin"), properties.getProperty("web.adminPassword"));
     }
 
     public void stop() {
         wd.quit();
-    }
-
-    public void logout() {
-        click(By.linkText("Logout"));
-    }
-
-    private void click(By locator) {
-        wd.findElement(locator).click();
-    }
-
-    public GroupHelper group() {
-        return groupHelper;
-    }
-
-    public NavigationHelper goTo() {
-        return navigationHelper;
-    }
-
-    public ContactHelper contact() {
-        return contactHelper;
-    }
-
-    public DbHelper db() {
-        return dbHelper;
     }
 }
