1e9bd9481e0cecd8527a7953813c7cc86a5d20f2
irokonlywork <irokonlywork@users.noreply.github.com>
2020-07-11 22:29:56 +0300

    Получение почты на внешний почтовый сервер. Создание пользователей на внешнем почтовом сервере (по протоколу Telnet)

--- mantis-tests/src/test/java/ru/stqa/course/mantis/tests/RegistrationJamesTests.java
+++ mantis-tests/src/test/java/ru/stqa/course/mantis/tests/RegistrationJamesTests.java
@@ -1,32 +1,27 @@
 package ru.stqa.course.mantis.tests;
 
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ru.lanwen.verbalregex.VerbalExpression;
 import ru.stqa.course.mantis.model.MailMessage;
 
+import javax.mail.MessagingException;
 import java.io.IOException;
 import java.util.List;
 
 import static java.lang.String.format;
 import static org.testng.Assert.assertTrue;
 
-public class RegistrationTests extends TestBase { // с внутренним почтовым сервером
-
-    @BeforeMethod
-    public void startMailServer() {
-        app.mail().start();
-    }
+public class RegistrationJamesTests extends TestBase { // с внешним почтовым сервером
 
     @Test
-    public void testRegistration() throws IOException {
+    public void testRegistration() throws IOException, MessagingException {
         long now = System.currentTimeMillis();
         String user = format("user%s", now);
         String password = "password";
         String email = format("user%s@localhost.localdomain", now);
+        app.james().createUser(user, password);
         app.registration().start(user, email);
-        List<MailMessage> mailMessages = app.mail().waitForMail(2, 10000);
+        List<MailMessage> mailMessages = app.james().waitForMail(user, password, 60000);
         String confirmationLink = findConfirmationLink(mailMessages, email);
         app.registration().finish(confirmationLink, user, password);
         assertTrue(app.newSession().login(user, password));
@@ -38,8 +33,4 @@ public class RegistrationTests extends TestBase { // с внутренним п�
         return regex.getText(mailMessage.text);
     }
 
-    @AfterMethod(alwaysRun = true)
-    public void stopMailServer() {
-        app.mail().stop();
-    }
 }
